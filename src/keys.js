4ca176f4d7820288d1f624f4469520d90de81a2e
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2019-10-05 14:30:59 -0500

    .

--- src/keys.js
+++ src/keys.js
@@ -4,8 +4,8 @@ module.exports = {
         connectionLimit: 10,
         host: 'localhost',
         user: 'root',
-        password: 'Password1',
-        database: 'db_links'
+        password: '',
+        database: 'Marketing'
     }
 
 };
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+
 const passport = require('passport');
 const { isLoggedIn } = require('../lib/auth');
 
