fabcd634823fb857625b3000affc02f966f9f507
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2019-10-12 02:53:40 -0500

    terminado

--- src/public/scripts/chart.js
+++ src/public/scripts/chart.js
@@ -1,64 +1,112 @@
-// Load Charts and the corechart package.
-google.charts.load("current", { packages: ["corechart"] });
+google.charts.load("current", {
+  callback: function() {
+    var arrayData = [];
+    var arrayViewsFinal = [];
+    var arrayVisitsFinal = [];
+    var arrayLikesFinal = [];
+    $.get("/data/data2.csv", function(csvString) {
+      arrayData = $.csv.toArrays(csvString, {
+        onParseValue: $.csv.hooks.castToScalar,
+        separator: "|"
+      });
 
-// Draw the pie chart for Sarah's pizza when Charts is loaded.
-google.charts.setOnLoadCallback(drawSarahChart);
+      console.log("Array despues de llamar el archivo");
+      console.log(arrayData);
 
-// Draw the pie chart for the Anthony's pizza when Charts is loaded.
-google.charts.setOnLoadCallback(drawAnthonyChart);
+      var arrayDataViews = $.csv.toArrays(arrayData[0][0], {
+        onParseValue: $.csv.hooks.castToScalar,
+        separator: "-"
+      });
+      var arrayDataVisits = $.csv.toArrays(arrayData[0][1], {
+        onParseValue: $.csv.hooks.castToScalar,
+        separator: "-"
+      });
+      var arrayDataLikes = $.csv.toArrays(arrayData[0][2], {
+        onParseValue: $.csv.hooks.castToScalar,
+        separator: "-"
+      });
 
-// Callback that draws the pie chart for Sarah's pizza.
-function drawSarahChart() {
-  // Create the data table for Sarah's pizza.
-  var data = new google.visualization.DataTable();
-  data.addColumn("string", "Topping");
-  data.addColumn("number", "Slices");
-  data.addRows([
-    ["Mushrooms", 1],
-    ["Onions", 1],
-    ["Olives", 2],
-    ["Zucchini", 2],
-    ["Pepperoni", 1]
-  ]);
+      console.log("Textos convertidos en array");
+      console.log(arrayDataViews[0]);
+      console.log(arrayDataVisits[0]);
+      console.log(arrayDataLikes[0]);
 
-  // Set options for Sarah's pie chart.
-  var options = {
-    title: "How Much Pizza Sarah Ate Last Night",
-    width: 400,
-    height: 300
-  };
+      var arrayViewsRes = $.csv.toArrays(arrayDataViews[0][0], {
+        onParseValue: $.csv.hooks.castToScalar,
+        delimiter: ","
+      });
+      var arrayViews = $.csv.toArrays(arrayDataViews[0][1], {
+        onParseValue: $.csv.hooks.castToScalar,
+        delimiter: ","
+      });
 
-  // Instantiate and draw the chart for Sarah's pizza.
-  var chart = new google.visualization.PieChart(
-    document.getElementById("Sarah_chart_div")
-  );
-  chart.draw(data, options);
-}
+      var arrayVisitsRes = $.csv.toArrays(arrayDataVisits[0][0], {
+        onParseValue: $.csv.hooks.castToScalar,
+        delimiter: ","
+      });
+      var arrayVisits = $.csv.toArrays(arrayDataVisits[0][1], {
+        onParseValue: $.csv.hooks.castToScalar,
+        delimiter: ","
+      });
 
-// Callback that draws the pie chart for Anthony's pizza.
-function drawAnthonyChart() {
-  // Create the data table for Anthony's pizza.
-  var data = new google.visualization.DataTable();
-  data.addColumn("string", "Topping");
-  data.addColumn("number", "Slices");
-  data.addRows([
-    ["Mushrooms", 2],
-    ["Onions", 2],
-    ["Olives", 2],
-    ["Zucchini", 0],
-    ["Pepperoni", 3]
-  ]);
+      var arrayLikesRes = $.csv.toArrays(arrayDataLikes[0][0], {
+        onParseValue: $.csv.hooks.castToScalar,
+        delimiter: ","
+      });
+      var arrayLikes = $.csv.toArrays(arrayDataLikes[0][1], {
+        onParseValue: $.csv.hooks.castToScalar,
+        delimiter: ","
+      });
 
-  // Set options for Anthony's pie chart.
-  var options = {
-    title: "How Much Pizza Anthony Ate Last Night",
-    width: 400,
-    height: 300
-  };
+      console.log("Textos convertidos final");
+      console.log("Views");
+      console.log(arrayViewsRes);
+      console.log(arrayViews);
+      console.log("Visits");
+      console.log(arrayVisitsRes);
+      console.log(arrayVisits);
+      console.log("Likes");
+      console.log(arrayLikesRes);
+      console.log(arrayLikes);
 
-  // Instantiate and draw the chart for Anthony's pizza.
-  var chart = new google.visualization.PieChart(
-    document.getElementById("Anthony_chart_div")
-  );
-  chart.draw(data, options);
-}
+      arrayViewsFinal.push(arrayViewsRes[0]);
+      arrayViewsFinal.push(arrayViews[0]);
+
+      arrayVisitsFinal.push(arrayVisitsRes[0]);
+      arrayVisitsFinal.push(arrayVisits[0]);
+
+      arrayLikesFinal.push(arrayLikesRes[0]);
+      arrayLikesFinal.push(arrayLikes[0]);
+
+      console.log("Array final a usar");
+      console.log(arrayViewsFinal);
+      console.log(arrayVisitsFinal);
+      console.log(arrayLikesFinal);
+
+      var dataViews = new google.visualization.arrayToDataTable(
+        arrayViewsFinal
+      );
+      var dataVisits = new google.visualization.arrayToDataTable(
+        arrayVisitsFinal
+      );
+      var dataLikes = new google.visualization.arrayToDataTable(
+        arrayLikesFinal
+      );
+
+      var chartViews = new google.visualization.ColumnChart(
+        document.getElementById("chartViews")
+      );
+      var chartVisits = new google.visualization.ColumnChart(
+        document.getElementById("chartVisits")
+      );
+      var chartLikes = new google.visualization.ColumnChart(
+        document.getElementById("chartLikes")
+      );
+
+      chartViews.draw(dataViews);
+      chartVisits.draw(dataVisits);
+      chartLikes.draw(dataLikes);
+    });
+  },
+  packages: ["corechart"]
+});
